e01d52d09a70b2c8af590ba4f0a2341ee5c72391
oSabre <txagabriel@gmail.com>
2022-04-24 16:53:35 -0300

    Atualização 2

--- PD_RMI_VELHA_CLIENT/src/rmi_velha/VelhaClient.java
+++ PD_RMI_VELHA_CLIENT/src/rmi_velha/VelhaClient.java
@@ -4,7 +4,8 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class VelhaClient extends UnicastRemoteObject implements VelhaClientInterface{
-
+	volatile private int tab[][] = new int [3][3];
+	
 	protected VelhaClient() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
@@ -12,7 +13,7 @@ public class VelhaClient extends UnicastRemoteObject implements VelhaClientInter
 
 	@Override
 	public void PrintBoard(int[][] board) throws RemoteException {
-		// TODO Auto-generated method stub
+		tab = board;
 		
 	}
 
